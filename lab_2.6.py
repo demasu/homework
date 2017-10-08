e995cdc19c89c1977150e9b834f68dcfb76dae35
Joshua Brandt <joshua.brandt@cpanel.net>
2017-10-08 17:42:08 -0500

    Finished program

--- lab_2.6.py
+++ lab_2.6.py
@@ -5,38 +5,35 @@
 #the main module
 def main():
     print 'Tire Service Calculator Program \n' # \n
-    print #prints a blank line
-    serviceCost = input('Enter the tire service cost$')
-    serviceCost = float(serviceCost)
-    return serviceCost
-    #serviceFee = calcServiceFee(serviceCost)
+    serviceCost = inputTireServiceCost()
+    serviceFee = calcServiceFee(serviceCost)
+    serviceTax = calcServiceTax(serviceCost)
+    serviceTotal = calcServiceTotal(serviceCost, serviceFee, serviceTax)
+    printUserInfo(serviceCost, serviceFee, serviceTax, serviceTotal)
 
-    #input statements
-    serviceCost = input('The service cost is: ')
-    #input statements
-    serviceFee = input('The service fee is: ')
    
 
 #this module will input tire service price
 def inputTireServiceCost():
-    return
+    serviceCost = input('Enter the tire service cost $')
+    serviceCost = float(serviceCost)
+    return serviceCost
 
     
 #this module will calculate a service fee at 5%
 def calcServiceFee(serviceCost):
-    #serviceFee = serviceCost * .05
+    serviceFee = serviceCost * .05
     return serviceFee
     
 #this module will calculate serviceTax at 8.25%
 def calcServiceTax(serviceCost):
-    #totalServiceCost = calcServiceTotal (serviceCost, serviceFee, serviceTax)
-    #serviceTax = serviceCost * .0825
+    serviceTax = serviceCost * .0825
     return serviceTax
 
 #this module will calculate serviceFee, serviceTax,
 #and the serviceTotal cost
 def calcServiceTotal(serviceCost, serviceFee, serviceTax):
-    #serviceTotal = serviceCost + serviceFee + serviceTax
+    serviceTotal = serviceCost + serviceFee + serviceTax
     return serviceTotal
 
 #this module will print tireServiceCost, serviceFee,
@@ -45,7 +42,7 @@ def printUserInfo(serviceCost, serviceFee, serviceTax, serviceTotal):
     print 'The tire service cost is $', serviceCost
     print 'The tire service fee is $', serviceFee
     print 'The tire service tax is $', serviceTax
-    print 'The total amount is $', serviceTotal
+    print 'The total amount is $', round(serviceTotal,2)
    
 
 
